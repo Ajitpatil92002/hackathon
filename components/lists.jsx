463e891f244e5f52246b9d89ed416f3249dd8871
Ajit Patil <patilajit020@gmail.com>
2023-10-21 09:40:43 +0530

    added alert box if user wallect not connected

--- components/lists.jsx
+++ components/lists.jsx
@@ -38,7 +38,12 @@ const Lists = () => {
           </div>
         </>
       ) : (
-        "please connect you wallet"
+        <div
+          class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert"
+        >
+          <strong class="font-bold">Please connect your wallet</strong>
+        </div>
       )}
       {maintenanceRequests &&
         maintenanceRequests.map((ml) => (
